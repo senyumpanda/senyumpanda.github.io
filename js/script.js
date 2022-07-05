2ca083067e365539a2c6aa587fe55e30960525b2
senyumpanda <qubarokah@gmail.com>
2022-07-06 01:38:56 +0700

    memperbaharui data

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+// Navbar
+window.onscroll = () => {
+    const header = document.querySelector("header");
+    const fixedTopNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedTopNav) {
+        header.classList.add("navbar-fixed")
+    } else {
+        header.classList.add("navbar-fixed")
+    }
+}
+
+// Hamburger
 const hamburger = document.getElementById("hamburger");
 const nav_menu = document.getElementById("nav-menu");
 
